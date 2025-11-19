020aa2c75b1790c0f374ad11bb1509883305ce7d
Ze-Well-Souza <wellington.0301@hotmail.com>
2025-11-19 16:35:57 -0300

    alguns ajustes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ajd1vl93ef8"
+    "revision": "0.e5u821mvgrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
